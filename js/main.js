7e6e94c590a499b76d52e54e3082b63bb116632d
Елизавета Софронова <liza.sofronova.99@gmail.com>
2024-08-16 01:49:49 +0300

    отправит задание №4.16

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ const getRandomElement = (elements) => {
   return elements[getRandomNumber(0, elements.length - 1)];
 };
 
-const getRandomInteger = () => {
+const getRandomInt = () => {
   return Math.floor(Math.random() * 100);
 };
 
@@ -56,7 +56,7 @@ const createDescription = () => {
     description: getRandomElement(DESCRIPTIONS),
     likes: getRandom(15, 200),
     comments: {
-      id: getRandomInteger(),
+      id: getRandomInt(),
       avatar: 'img/avatar-' +  getRandom(1, 6) + '.svg',
       message: getRandomElement(MESSAGES),
       name: getRandomElement(NAMES),
